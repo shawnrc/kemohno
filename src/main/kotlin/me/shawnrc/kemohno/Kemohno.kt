4498f8504545aa6a298fa3466030bc20b9a9a657
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-05-26 01:22:30 -0400

    temporary post endpoint

--- src/main/kotlin/me/shawnrc/kemohno/Kemohno.kt
+++ src/main/kotlin/me/shawnrc/kemohno/Kemohno.kt
@@ -1,17 +1,25 @@
 package me.shawnrc.kemohno
 
 import spark.kotlin.ignite
+import java.awt.SystemColor.text
 
 fun main(args: Array<String>) {
   val config = getConfig()
-  val http = ignite()
-  http.port(config.port)
+  ignite().apply {
+    port(config.port)
 
-  http.get("lad") {
-    "bungus\n"
-  }
-  http.post("/bepis") {
+    get("hello") {
+      "hello"
+    }
 
+    post("/bepis") {
+      val userId = request.params("user_id")
+      mapOf(
+          "response_type" to "in_channel",
+          "text" to "wnelo",
+          "as_user" to userId
+      )
+    }
   }
 }
 
