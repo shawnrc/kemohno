4217b989bc29c5c6f95a82ba94c5c583c86239a1
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-05-27 12:43:43 -0400

    tracks used emoji to reduce duplicates

--- src/main/kotlin/me/shawnrc/kemohno/Emojifier.kt
+++ src/main/kotlin/me/shawnrc/kemohno/Emojifier.kt
@@ -4,6 +4,7 @@ import com.beust.klaxon.Klaxon
 import java.io.File
 import java.nio.charset.Charset
 import java.util.*
+import kotlin.math.min
 
 class Emojifier(emojiFile: File) {
   private val random = Random()
@@ -13,25 +14,37 @@ class Emojifier(emojiFile: File) {
   fun translate(string: String): String = buildString {
     val normalized = string.toUpperCase()
     var index = 0
+    var retries = 0
+    val usedEmoji = mutableSetOf<String>()
     while (index < normalized.length) {
       val skipIndex = index + 3
       if (normalized.bemojiAt(index, skipIndex)) {
         append(":b:")
-        index = skipIndex
         continue
       }
       val currentLetter = normalized[index].toString()
       val letterPool = emojiMap.array<String>(currentLetter)
-      append(letterPool?.randomItem() ?: currentLetter)
+      val selected = letterPool?.randomItem() ?: currentLetter
+      if (selected.isEmoji()) {
+        if (selected in usedEmoji && retries < 2) {
+          ++retries
+          continue
+        }
+        usedEmoji.add(selected)
+        retries = 0
+      }
+      append(selected)
 
       ++index
     }
   }
 
   private fun String.bemojiAt(start: Int, end: Int): Boolean {
-    return this[start] == ':'
-        && end < length
-        && substring(start until end) == ":B:"
+    return substring(start until min(end, length)) == ":B:"
+  }
+
+  private fun String.isEmoji(): Boolean {
+    return startsWith(':') && endsWith(':')
   }
 
   private fun <E> List<E>.randomItem(): E {
